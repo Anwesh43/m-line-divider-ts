41481f3f773be3966e9b3e8367f8be512e48d326
Anwesh43 <anweshthecool0@gmail.com>
2020-12-28 10:34:39 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,39 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawMLineDivider(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        context.save()
+        context.translate(w / 2, h)
+        DrawingUtil.drawLine(context, 9, 9, 0, -size * sf1)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, -size, -size * sf2, -size * (1 + sf2))
+            DrawingUtil.drawLine(context, -size, 0, -size, -2 * size * sf3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawMLDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawMLineDivider(context, i)
+    }
+}
