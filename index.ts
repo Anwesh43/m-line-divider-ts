a13876884c157c5ed08fdcee6f7213e0ac3d7858
Anwesh43 <anweshthecool0@gmail.com>
2020-12-28 11:24:44 +0530

    adding MLDNode class

--- index.ts
+++ index.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class MLDNode {
+
+    next : MLDNode 
+    prev : MLDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MLDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMLDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : MLDNode {
+        var curr : MLDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
